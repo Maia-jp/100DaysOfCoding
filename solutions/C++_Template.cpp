96e64d74a06f2577ce2b75700a7237bc24e9d914
JP Maia <57457490+Maia-jp@users.noreply.github.com>
2021-06-21 11:00:09 -0300

    Create C++_Template.cpp

---
+++ solutions/C++_Template.cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+    Competitive Programming Template
+    by: Joao P Maia
+    compile as: g++ -std=c++11 -O2 -Wall code.cpp -o bin
+    Online Compiler: https://onlinegdb.com/VXHr9UG44
+*******************************************************************************/
+
+/******************************************************************************
+    ---Complexity guide---
+    
+n ≤ 10 :=: O(n!)
+n ≤ 20 :=: O(2n)
+n ≤ 500 :=: O(n^3)
+n ≤ 5000 :=: O(n^2)
+n ≤ 106 :=: O(nlogn) or O(n)
+n is large :=: O(1) or O(logn)
+*******************************************************************************/
+
+/*Libary*/
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+/*MACROS - uncomment if need it*/
+//#define MIN(a,b) ( ( ( a ) < ( b ) )  ? a : b )
+//#define MAX(a,b) ( ( ( a ) > ( b ) )  ? a : b )
+//#define F first
+//#define S second
+//#define PB push_back
+//#define MP make_pair
+//#define REP(i,a,b) for (int i = a; i <= b; i++)
+//#define SORTV(a) sort(a.begin(),a.end()) 
+//#define SORTA(a,n) sort(a,a+n)
+//#define MAXA(a,n) *max_element(a,a+n) 
+//#define MINA(a,n) *min_element(a,a+n) 
+//#define REV(v) reverse(v.begin(),v.end()) 
+//#define SUM(v) accumulate(v.begin(),v.end(),0) 
+//#define MAXV(v) *max_element(v.begin(),v.end()) 
+//#define MINV(v) *min_element(v.begin(),v.end())
+
+/*DEBBUGIN - uncomment if need it*/
+//inline void pisz(int n) { printf("%d\n",n); }
+//#define DBG(vari) cerr<<#vari<<" = "<<(vari)<<endl;
+//#define printA(a,L,R) FE(i,L,R) cout << a[i] << (i==R?'\n':' ')
+//#define printV(a) printA(a,0,a.size()-1)
+//#define MAXN 10000
+
+
+using namespace std;
+#define IOS ios_base::sync_with_stdio(0); //to synchronize the input of cin and scanf
+int main()
+{
+    printf("Hello World");
+
+
+
+
+    return 0;
+}
